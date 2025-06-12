109e5eae4df7c0d126d9be4131cc7762343c5497
Rikki-Wallis <rikkiwallis2004@gmail.com>
2025-06-13 03:45:02 +1000

    change how the game simulates a game

--- classes/game.py
+++ classes/game.py
@@ -25,75 +25,58 @@ class Game:
         self.pointProgression = [0, 15, 30, 40, ("Hold", "Break")]
         self.winner = None
 
-    def simulate_point(self):
-        """
-        Method:
-            Simulates a point between the two players based off of
-            probabilities obtained from the Markov Chain, While the we
-            find the probabilities of transition to each absorption state,
-            this function will be used along with monte-carlo to verify
-            the probabilities of both stochastic methods.
-
-        Return:
-            updatedScore (tuple(int|str,int|str)): The updated score after point simulation
-        """
-        # Fetching each players points
-        serverPoints = self.score[0]
-        returnerPoints = self.score[1]
-
-        # Fetching target scores for both players
-        serverTargetIndex = self.pointProgression.index(serverPoints) + 1
-        returnerTargetIndex = self.pointProgression.index(returnerPoints) + 1
-        serverTargetScore = self.pointProgression[serverTargetIndex]
-        returnerTargetScore = self.pointProgression[returnerTargetIndex]
-
-        # Checking whether the target score results in a hold or a break
-        if serverTargetIndex == len(self.pointProgression) - 1:
-            serverTargetScore = "Hold"
-        elif returnerTargetIndex == len(self.pointProgression) - 1:
-            returnerTargetScore = "Break"
-
-        # Fetching probability that server wins the point
-        currentMarkovIndex = self.markovChainIndex[
-            self.format_score(serverPoints, returnerPoints)
-        ]
-        targetMarkovIndex = self.markovChainIndex[
-            self.format_score(serverTargetScore, returnerPoints)
-        ]
-        serverProb = self.markovChainIndex[currentMarkovIndex, targetMarkovIndex]
-
-        # Randomly sampling to see if server wins the point
-        sample = np.random.uniform(low=0, high=1)
-
-        # Updating score based on random sample
-        if sample <= serverProb:
-            updatedScore = (serverTargetScore, returnerPoints)
-        else:
-            updatedScore = (serverPoints, returnerTargetScore)
-
-        # If the updated score results in deuce we need to set it to 30-30 so markov indexing works
-        if updatedScore == (40, 40):
-            updatedScore = (30, 30)
-
-        return updatedScore
-
     def simulate_game(self):
         """
         Method:
             Simulates the game
         """
-        while self.winner == None:
-            # Simulate the point
-            updatedScore = self.simulate_point()
+        startIndex = self.markovChainIndex["0-0"]
+        holdIndex = self.markovChainIndex["Hold"]
+        
+        B, transient_indices, absorbing_indices = self.compute_absorption_probabilities(self.markovChain)
 
-            # If the game has a winner update to exit loop
-            if "Hold" in updatedScore:
-                self.winner = self.server
-            elif "Break" in updatedScore:
-                self.winner = self.returner
+        start_i = np.where(transient_indices == startIndex)[0][0]
+        hold_j = np.where(absorbing_indices == holdIndex)[0][0]
 
-            # Update score
-            self.score = updatedScore
+        sWin = B[start_i, hold_j]
+
+        if np.random.uniform(0,1) <= sWin:
+            self.winner = self.server
+        else:
+            self.winner = self.returner
+
+
+    def compute_absorption_probabilities(self, P):
+        """
+        Compute the absorption probabilities for a Markov chain with absorbing states.
+        
+        Args:
+            P (np.ndarray): Transition matrix with absorbing states at the end.
+
+        Returns:
+            B (np.ndarray): Matrix of absorption probabilities where B[i, j] is the probability
+                            of ending in absorbing state j from transient state i.
+        """
+        n = P.shape[0]
+
+        # Identify absorbing states: rows with 1 on the diagonal and zeros elsewhere
+        absorbing = np.isclose(P, np.eye(n)).all(axis=1)
+
+        # Partition the matrix into Q and R
+        transient_indices = np.where(~absorbing)[0]
+        absorbing_indices = np.where(absorbing)[0]
+
+        Q = P[np.ix_(transient_indices, transient_indices)]
+        R = P[np.ix_(transient_indices, absorbing_indices)]
+
+        # Fundamental matrix
+        I = np.eye(Q.shape[0])
+        N = np.linalg.inv(I - Q)
+
+        # Absorption probabilities
+        B = N @ R
+
+        return B, transient_indices, absorbing_indices
 
     def generate_markov_chain(self):
         """
@@ -107,10 +90,7 @@ class Game:
             markovChain (List(List(float))): The generated markov chain
         """
         # Obtaining probability that server will win each point based on player attributes
-        sWin = (self.server.serveStrength * self.server.form) / (
-            self.server.serveStrength * self.server.form
-            + self.returner.returnStrength * self.returner.form
-        )
+        sWin = (self.server.serveStrength * self.server.form) / (self.server.serveStrength * self.server.form + self.returner.returnStrength * self.returner.form )
 
         # The probability that the returner will win the point is just the compliment of the server's probability
         rWin = 1 - sWin
@@ -118,106 +98,54 @@ class Game:
         # Creating Markov chain
         markovChain = np.array(
             [  # 0-0   15-0    30-0    40-0    0-15    0-30    0-40    15-15   30-15   40-15   15-30   15-40   30-30   40-30   30-40   Hold    Break
-                [0, sWin, 0, 0, rWin, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],  # 0-0
-                [0, 0, sWin, 0, 0, 0, 0, rWin, 0, 0, 0, 0, 0, 0, 0, 0, 0],  # 15-0
-                [0, 0, 0, sWin, 0, 0, 0, 0, rWin, 0, 0, 0, 0, 0, 0, 0, 0],  # 30-0
-                [0, 0, 0, 0, 0, 0, 0, 0, 0, rWin, 0, 0, 0, 0, 0, sWin, 0],  # 40-0
-                [0, 0, 0, 0, 0, rWin, 0, sWin, 0, 0, 0, 0, 0, 0, 0, 0, 0],  # 0-15
-                [0, 0, 0, 0, 0, 0, rWin, 0, 0, 0, sWin, 0, 0, 0, 0, 0, 0],  # 0-30
-                [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, sWin, 0, 0, 0, 0, rWin],  # 0-40
-                [0, 0, 0, 0, 0, 0, 0, 0, sWin, 0, rWin, 0, 0, 0, 0, 0, 0],  # 15-15
-                [0, 0, 0, 0, 0, 0, 0, 0, 0, sWin, 0, 0, rWin, 0, 0, 0, 0],  # 30-15
-                [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, rWin, 0, sWin, 0],  # 40-15
-                [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, rWin, sWin, 0, 0, 0, 0],  # 15-30
-                [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, sWin, 0, rWin],  # 15-40
-                [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, sWin, rWin, 0, 0],  # 30-30
-                [
-                    0,
-                    0,
-                    0,
-                    0,
-                    0,
-                    0,
-                    0,
-                    0,
-                    0,
-                    0,
-                    0,
-                    0,
-                    rWin,
-                    0,
-                    0,
-                    sWin,
-                    0,
-                ],  # 40-30 (A-40)
-                [
-                    0,
-                    0,
-                    0,
-                    0,
-                    0,
-                    0,
-                    0,
-                    0,
-                    0,
-                    0,
-                    0,
-                    0,
-                    sWin,
-                    0,
-                    0,
-                    0,
-                    rWin,
-                ],  # 30-40 (40-A)
-                [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0],  # Hold
-                [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],  # Break
+                [0,     sWin,    0,      0,    rWin,     0,     0,       0,      0,      0,      0,      0,      0,      0,      0,      0,      0],  # 0-0
+                [0,     0,     sWin,     0,      0,      0,     0,       rWin,   0,      0,      0,      0,      0,      0,      0,      0,      0],  # 15-0
+                [0,     0,       0,    sWin,     0,      0,     0,       0,      rWin,   0,      0,      0,      0,      0,      0,      0,      0],  # 30-0
+                [0,     0,       0,      0,      0,      0,     0,       0,      0,    rWin,     0,      0,      0,      0,      0,    sWin,     0],  # 40-0
+                [0,     0,       0,      0,      0,    rWin,    0,      sWin,    0,      0,      0,      0,      0,      0,      0,      0,      0],  # 0-15
+                [0,     0,       0,      0,      0,      0,    rWin,     0,      0,      0,    sWin,     0,      0,      0,      0,      0,      0],  # 0-30
+                [0,     0,       0,      0,      0,      0,     0,       0,      0,      0,      0,    sWin,     0,      0,      0,      0,   rWin],  # 0-40
+                [0,     0,       0,      0,      0,      0,     0,       0,     sWin,    0,    rWin,     0,      0,      0,      0,      0,      0],  # 15-15
+                [0,     0,       0,      0,      0,      0,     0,       0,      0,     sWin,    0,      0,     rWin,    0,      0,      0,      0],  # 30-15
+                [0,     0,       0,      0,      0,      0,     0,       0,      0,      0,      0,      0,      0,     rWin,    0,     sWin,    0],  # 40-15
+                [0,     0,       0,      0,      0,      0,     0,       0,      0,      0,      0,     rWin,   sWin,    0,      0,      0,      0],  # 15-30
+                [0,     0,       0,      0,      0,      0,     0,       0,      0,      0,      0,      0,      0,      0,     sWin,    0,   rWin],  # 15-40
+                [0,     0,       0,      0,      0,      0,     0,       0,      0,      0,      0,      0,      0,     sWin,   rWin,    0,      0],  # 30-30
+                [0,     0,       0,      0,      0,      0,     0,       0,      0,      0,      0,      0,     rWin,    0,      0,     sWin,    0],  # 40-30 (A-40)
+                [0,     0,       0,      0,      0,      0,     0,       0,      0,      0,      0,      0,     sWin,    0,      0,      0,   rWin],  # 30-40 (40-A)
+                [0,     0,       0,      0,      0,      0,     0,       0,      0,      0,      0,      0,      0,      0,      0,      1,     0 ],  # Hold
+                [0,     0,       0,      0,      0,      0,     0,       0,      0,      0,      0,      0,      0,      0,      0,      0,     1 ],  # Break
             ]
         )
 
         return markovChain
-
+    
     def generate_markov_chain_index(self):
-        """
-        Method:
-            Returns a dictionary of the indexes for the markov
-            chain given a score
+            """
+            Method:
+                Returns a dictionary of the indexes for the markov
+                chain given a score
 
-        Return:
-            markovIndexes (dict)
-        """
-        return {
-            "0-0": 0,
-            "15-0": 1,
-            "30-0": 2,
-            "40-0": 3,
-            "0-15": 4,
-            "0-30": 5,
-            "0-40": 6,
-            "15-15": 7,
-            "30-15": 8,
-            "40-15": 9,
-            "15-30": 10,
-            "15-40": 11,
-            "30-30": 12,
-            "40-30": 13,
-            "30-40": 14,
-            "Hold": 15,
-            "Break": 16,
-        }
+            Return:
+                markovIndexes (dict)
+            """
+            return {
+                "0-0": 0,
+                "15-0": 1,
+                "30-0": 2,
+                "40-0": 3,
+                "0-15": 4,
+                "0-30": 5,
+                "0-40": 6,
+                "15-15": 7,
+                "30-15": 8,
+                "40-15": 9,
+                "15-30": 10,
+                "15-40": 11,
+                "30-30": 12,
+                "40-30": 13,
+                "30-40": 14,
+                "Hold": 15,
+                "Break": 16,
+            }
 
-    def format_score(self, score=None):
-        """
-        Method:
-            Formats a score to a string
-
-        Params:
-            score (None|tuple(int|str, int|str)): The score that will be formatted. If none,
-                                                  formats the game classes own score.
-
-        Return:
-            formattedScore (str): The formatted score
-        """
-        if score != None:
-            return f"{score[0]}-{score[1]}"
-        else:
-            return f"{self.score[0]}-{self.score[1]}"
