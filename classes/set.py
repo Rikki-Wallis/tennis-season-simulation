1d3c5574081056d362e8ef867ac356dea2b88e68
Rikki-Wallis <rikkiwallis2004@gmail.com>
2025-06-13 04:28:54 +1000

    Minor fixes to set class

--- classes/set.py
+++ classes/set.py
@@ -1,6 +1,6 @@
 # Imports
 from game import Game
-
+from tiebreak import Tiebreak
 
 class Set:
     def __init__(self, startingServer, startingReturner):
@@ -14,10 +14,9 @@ class Set:
             startingServer (Player): The player that starts serving in the match
             startingReturner (Player): The player that starts returning in the match
         """
-        self.player1 = startingServer
-        self.player2 = startingReturner
-        self.playersList = [self.player1, self.player2]
-        self.score = {self.player1: 0, self.player2: 0}
+        self.server = startingServer
+        self.returner = startingReturner
+        self.score = [0, 0]
         self.winner = None
 
     def simulate_set(self):
@@ -28,53 +27,55 @@ class Set:
         Return:
             nextServer (tuple(Player)): the player that serves next idx 0 & returner at idx 1
         """
-        # Setting server and returner index for playersList
-        serverIndex = 0
-        returnerIndex = 1
         # Initiating game list
         self.gameList = []
 
         # Simulating the set
         while self.winner == None:
             # Creating game object
-            game = Game(self.playersList[serverIndex], self.playersList[returnerIndex])
+            game = Game(self.server, self.returner)
 
             # Simulate the game
             game.simulate_game()
 
-            # Tracking games
-            self.gameList.append(game)
-
             # Getting winner and updating score
-            gameWinner = game.winner
-            self.score[gameWinner] += 1
+            winner = game.winner
+            if winner == self.server:
+                self.score[0] += 1
+            else:
+                self.score[1] += 1
 
             # If the set has a game score that wins at 6 games
-            if max(self.score) == 6 and (self.score != (6, 6) or self.score != (6, 5)):
+            if max(self.score) == 6 and (self.score != (6, 6) or self.score != (6, 5) or self.score != (5,6)):
                 # Updating the set win for the right player
-                if self.score(0) == 6:
-                    self.winner = self.player1
+                if self.score[0] == 6:
+                    self.winner = self.server
                 else:
-                    self.winner = self.player2
+                    self.winner = self.returner
 
             # If the game score is 7-5 we should select winner appropriatly
-            elif self.score == (7, 5) or self.score == (5, 7):
-                # Updating winner
-                if self.score[0] == 7:
-                    self.winner = self.player1
-                else:
-                    self.winner = self.player2
+            elif self.score == (7, 5):
+                self.winner = self.server
+            elif self.score == (5, 7):
+                self.winner = self.returner 
 
             # If it is a tiebreak simulate accordingly
             elif self.score == (6, 6):
-                """
-                Figure out tiebreak class
-                """
+                # Simulate tiebreak
+                tiebreak = Tiebreak(self.server, self.returner)
+                tiebreak.simulate_tiebreak()
+                
+                # Update winner and score
+                if tiebreak.winner == self.server:
+                    self.score[0] += 1
+                else:
+                    self.score[1] += 1
+                
+                self.winner = tiebreak.winner
 
-            else:
-                pass
-
-            serverIndex = 0 if serverIndex == 1 else 1
-            returnerIndex = 0 if returnerIndex == 1 else 1
-
-        return (self.playersList[serverIndex], self.playersList[returnerIndex])
+            # swap server and return and update score to match
+            self.server, self.returner = self.returner, self.server
+            
+            self.score = [self.score[1], self.score[0]]
+                
+        return (self.server, self.returner)
